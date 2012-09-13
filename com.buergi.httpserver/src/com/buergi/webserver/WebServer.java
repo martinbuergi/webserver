09d300d198f3240fce18f54cc7d941d764925c8f
Martin Bürgi <martin.buergi@idparc.ch>
2012-09-13 18:41:16 +0200

    ddd

--- com.buergi.httpserver/src/com/buergi/webserver/WebServer.java
+++ com.buergi.httpserver/src/com/buergi/webserver/WebServer.java
@@ -1,13 +1,17 @@
 package com.buergi.webserver;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+
 import com.buergi.webserver.http.impl.HttpRequest10Impl;
 import com.buergi.webserver.http.impl.HttpRequest11Impl;
-import com.buergi.webserver.services.HttpResponseService;
 import com.buergi.webserver.services.HttpRequestParserService;
+import com.buergi.webserver.services.HttpResponseService;
 import com.buergi.webserver.services.WebServerService;
 import com.buergi.webserver.services.WorkerService;
-import com.buergi.webserver.services.impl.HttpResponseServiceImpl;
 import com.buergi.webserver.services.impl.HttpRequestParserServiceImpl;
+import com.buergi.webserver.services.impl.HttpResponseServiceImpl;
 import com.buergi.webserver.services.impl.WebServerServiceImpl;
 import com.buergi.webserver.services.impl.WebServerServiceImpl.ServerBufferSize;
 import com.buergi.webserver.services.impl.WebServerServiceImpl.ServerDocRoot;
@@ -26,20 +30,26 @@ public class WebServer extends AbstractModule {
 	public static void main(String[] args) {
 		int port = 8080;
 		int bufferSize = 2*1024;
-
-		if (args.length == 0) {
-            System.err.println("Please specify document root directory");
-            System.exit(1);
-          }
-			
-		String docRoot = args[0];
+		String docRoot = null;
 		
-		if (args.length >= 2)
-			port = Integer.valueOf(args[1]);
-		if (args.length >= 3)
-			bufferSize = Integer.valueOf(args[2]);
-
-		
+		try {
+			 PropertiesConfiguration config = new PropertiesConfiguration(WebServer.class.getResource("properties.config"));
+			 if (!config.containsKey("root") || (docRoot = config.getString("root")).length() == 0){
+		            System.err.println("Please specify document root directory in properties.config (Default is user.home)");
+					 docRoot = System.getProperty("user.home");
+			 }
+			 
+			 if (config.containsKey("port"))
+				 port = config.getInt("port");
+	
+			 if (config.containsKey("buffer"))
+				 bufferSize = config.getInt("buffer");
+			 
+		} catch (ConfigurationException e) {
+			System.err.println("Configfile error: " + e.getMessage());
+			System.exit(1);
+		}
+	
 		Injector injector = Guice.createInjector(new HttpRequest10Impl(), new HttpRequest11Impl(), new WebServer(docRoot, port, bufferSize));
 		WebServerService httpServer = injector.getInstance(WebServerService.class);
 		httpServer.start();				
